80e999a4004cd5935e3a7ad58293c32a316a5af7
yuzu <yuzukwok@gmail.com>
2024-10-05 12:05:06 +0800

    init

---
+++ RAZ-MP3-Rename/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.ObjectModel;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace RAZ;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var content= File.ReadAllText("ouput.txt");
+        
+        //model
+       // {"quizStars":0,"isPlanLocked":false,"id":2814,"readStars":0,"isFreeBook":false,"goldStars":0,"scene":["Others"],"sid":[0],"guest_readable":0,"pkStars":0,"selected":false,"isWords":false,"cat_name":"Z","is_new":true,"read_score_max":0,"number":0,"orientation":2,"interact":false,"zipfile":"https:\/\/qnfile.abctime.com\/bookzf_1555050137.zip","previewStars":0,"has_audio":false,"book_name":"The Queen's Loss (Part I)","themeName":"Others","preview_words":0,"is_finish":false,"themeId":"0","is_lock":false,"isRead":false,"cid":"29","topic":0,"words_num":1818,"isRecordingScore":false,"read_time":29,"pic":"https:\/\/qnfile.abctime.com\/book_1555042035.png","isQuiz":false}
+
+        
+        //提取文本中所有json字符
+        var jsons = Regex.Matches(content, @"\{.*?\}");
+        IList<model> add = new List<model>();
+        foreach (Match json in jsons)
+        {
+            var model = JsonSerializer.Deserialize<model>(json.Value);
+            if (model.cat_name=="E")
+            {
+               add.Add(model);
+            }
+        }
+        
+        add=add.OrderBy(x=>x.book_name).ToList();
+        
+        // mp3 files 
+        var mp3files=Directory.GetFiles("RAZ","*.mp3");
+        
+       var  filename=mp3files.Select(x=>new FileInfo(x).Name.ToUpper().Replace(" ","")).ToArray();
+        //index
+        for (int i = 0; i < add.Count; i++)
+        {
+            bool isMatch = false;
+            for (var index = 0; index < filename.Length; index++)
+            {
+               
+                var name = filename[index];
+                if (name.Contains(add[i].book_name.Replace(",","")
+                        .Replace("!","")
+                        .Replace("?","")
+                        .Replace("'","")
+                        .Replace("\"","")
+                        .Replace(" ","").ToUpper()))
+                {
+                   // Console.WriteLine($"{i + 1}-{add[i].book_name} has mp3");
+                   new FileInfo(mp3files[index]).CopyTo($"{i + 1} {add[i].book_name}.mp3",true);
+                   isMatch = true;
+
+                }
+                else
+                {
+                    // Console.WriteLine($"{i + 1}-{add[i].book_name} has no mp3");
+                }
+            }
+
+            if (!isMatch)
+            {
+                Console.WriteLine($"{i + 1}-{add[i].book_name} has no mp3");
+            }
+        }
+        
+        Console.WriteLine("Done");
+    }
+    
+    
+}
---
+++ RAZ-MP3-Rename/model.cs
@@ -0,0 +1,41 @@
+namespace RAZ;
+
+public class model
+{
+    public int quizStars { get; set; }
+    public bool isPlanLocked { get; set; }
+    public int id { get; set; }
+    public int readStars { get; set; }
+    public bool isFreeBook { get; set; }
+    public int goldStars { get; set; }
+    public string[] scene { get; set; }
+    public int[] sid { get; set; }
+    public int guest_readable { get; set; }
+    public int pkStars { get; set; }
+    public bool selected { get; set; }
+    public bool isWords { get; set; }
+    public string cat_name { get; set; }
+    public bool is_new { get; set; }
+    public int read_score_max { get; set; }
+    public int number { get; set; }
+    public int orientation { get; set; }
+    public bool interact { get; set; }
+    public string zipfile { get; set; }
+    public int previewStars { get; set; }
+    public bool has_audio { get; set; }
+    public string book_name { get; set; }
+    public string themeName { get; set; }
+    public int preview_words { get; set; }
+    public bool is_finish { get; set; }
+    public string themeId { get; set; }
+    public bool is_lock { get; set; }
+    public bool isRead { get; set; }
+    public string cid { get; set; }
+    public int topic { get; set; }
+    public int words_num { get; set; }
+    public bool isRecordingScore { get; set; }
+    public int read_time { get; set; }
+    public string pic { get; set; }
+    public bool isQuiz { get; set; }
+}
+
